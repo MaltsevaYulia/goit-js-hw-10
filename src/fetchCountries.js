d999f2409ef7700b676b7203fc4762ebf2d9f3dc
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-02-05 17:13:44 +0200

    Summary

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,14 @@
-const BASE_URL='https://restcountries.com/v3.1/name'
+const BASE_URL = 'https://restcountries.com/v3.1/name';
 
 function fetchCountries(name) {
-    return  fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
+  return fetch(
+    `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
 
-
-export {fetchCountries}
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -22,12 +22,6 @@ function onInputChange(evt) {
   }
   
   fetchCountries(country)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
     .then(renderListMarkup)
     .catch(onFetchError);
 }
